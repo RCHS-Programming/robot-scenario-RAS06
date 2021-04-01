c15767a1ef7aac8aa8825b2559ed802f176aaafb
RAS06 <rizwan.a.z.syed1996@gmail.com>
2021-04-01 08:26:17 -0400

    Finished off the last steps of the Robot Final Project
    Also did the optional portion because why not?

--- Robot.java
+++ Robot.java
@@ -1,3 +1,4 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -8,33 +9,49 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robot extends Actor
 {
+    private GreenfootImage robotimage1 =  new GreenfootImage("man01.png");
+    private GreenfootImage robotimage2 = new GreenfootImage("man02.png");
+    private GreenfootImage gameOver = new GreenfootImage("gameover.png");
+    
+    int imageSwitch = 10;
+    int lives = 3;
+    int score;
+    int pizzaEaten;
+    
     /**
      * Act - do whatever the Robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        eatPizza();
         robotMovement();
+        detectHome();
         detectWallCollision();
         detectBlockCollision();
+        showStatus();
     }    
         //Moves the Robot. 
     public void robotMovement() 
     {
         if (Greenfoot.isKeyDown("up")) 
         {
+            animate();
             setLocation(getX(), getY() - 3);
         }
         if (Greenfoot.isKeyDown("down")) 
         {
+            animate();
             setLocation(getX(), getY() + 3); 
         }
         if (Greenfoot.isKeyDown("left")) 
         {
+            animate();
             setLocation(getX() - 3, getY());
         }
         if (Greenfoot.isKeyDown("right")) 
         {
+            animate();
             setLocation(getX() + 3, getY());
         }
     }
@@ -44,6 +61,8 @@ public class Robot extends Actor
         if(isTouching(Wall.class))
         {
             setLocation(50, 50);
+            Greenfoot.playSound("hurt.wav");
+            removeLife();
         }
     }
         //Checks block collision. 
@@ -52,6 +71,68 @@ public class Robot extends Actor
         if(isTouching(Block.class))
         {
             setLocation(50, 50);
+            Greenfoot.playSound("hurt.wav");
+            removeLife();
         }
+    }
+        //Checks to see if the house was touched
+    public void detectHome () 
+    {
+        if(isTouching(Home.class))
+        {
+            //WHY ARE COMPOUND IF-STATEMENTS NOT WORKING?!?!
+            if(pizzaEaten == 5)
+            {
+                Greenfoot.playSound("yipee.wav");
+                setLocation(50, 50); 
+                score++;
+            }
+        }
+    }
+        //Checks to see if the robot has come across any food. 
+    public void eatPizza ()
+    {
+        if(isTouching(Pizza.class))
+        {
+            Greenfoot.playSound("eat.wav");
+            removeTouching(Pizza.class);
+            pizzaEaten++;
+        }
+    }
+        //Animates the walking of the robot. 
+    public void animate() 
+    {
+         if(imageSwitch >= 20) {
+            
+            if(getImage() == robotimage1) {
+                 setImage(robotimage2);
+                } else if (imageSwitch >= 40){
+                 setImage(robotimage1);
+                 imageSwitch = 0;
+                }
+                
+            }
+        imageSwitch++;
+    }
+        //Removes a life from the Robot. 
+    public void removeLife ()
+    {
+        lives--;
+        testEndGame();
+    }
+        //Tests to see if the robot has died. 
+    public void testEndGame()
+    {
+        if(lives <= 0)
+        {
+            greenfoot.Greenfoot.stop();
+            setImage(gameOver);
+        }
+    }
+        //Shows the properties of RObot. 
+    public void showStatus()  
+    {
+        getWorld().showText("Lives: " + lives, 70, 540);
+        getWorld().showText("Pizzas: " + pizzaEaten, 70, 560);
     }
 }
